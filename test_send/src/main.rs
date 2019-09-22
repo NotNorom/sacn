03590f84d73bce331b7d8949196b7c65785dc958
Paul <paul@lancasterzone.com>
2019-09-22 22:18:51 +0100

    Still working on multicast rcv

--- test_send/src/main.rs
+++ test_send/src/main.rs
@@ -1,9 +1,13 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate sacn;
 use sacn::DmxSource;
 use std::{thread, time}; // https://doc.rust-lang.org/std/thread/fn.sleep.html (20/09/2019)
 
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 fn main() {
-    let dmx_source = DmxSource::with_ip("Controller", "192.168.1.3").unwrap();
+    let dmx_source = DmxSource::with_ip("Controller", "192.168.1.6").unwrap();
 
     dmx_source.terminate_stream(1);
 
