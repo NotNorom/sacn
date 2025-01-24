cf2fad7ac6492c3dffae7752ebd2b93b34bf0cac
Andre Julius <noromoron@gmail.com>
2025-01-24 16:42:52 +0100

    migrate to 2021 edition

--- tests/ipv6_tests.rs
+++ tests/ipv6_tests.rs
@@ -1373,7 +1373,7 @@ mod sacn_ipv6_multicast_test {
                     }
                 }
                 Err(e) => {
-                    assert!(false, format!("Unexpected error returned: {:?}", e));
+                    assert!(false, "Unexpected error returned: {:?}", e);
                 }
             }
         }
