0b9da4b8d4583a11aa9ee0a2e237f66b1ee5256c
Andre Julius <noromoron@gmail.com>
2025-03-06 16:27:04 +0100

    A /24 subnet is not needed

--- sacn/further_testing/setup_ips.sh
+++ sacn/further_testing/setup_ips.sh
@@ -2,9 +2,9 @@
 
 export INTERFACE=$1;
 
-ip addr add dev $INTERFACE 192.168.0.6/24
-ip addr add dev $INTERFACE 192.168.0.7/24
-ip addr add dev $INTERFACE 192.168.0.8/24
+ip addr add dev $INTERFACE 192.168.0.6/32
+ip addr add dev $INTERFACE 192.168.0.7/32
+ip addr add dev $INTERFACE 192.168.0.8/32
 
 ip addr add dev $INTERFACE 2a02:c7f:d20a:c600:a502:2dae:7716:601b
 ip addr add dev $INTERFACE 2a02:c7f:d20a:c600:a502:2dae:7716:601c
